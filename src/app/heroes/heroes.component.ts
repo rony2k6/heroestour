55fc1f3a4e89a71eda7e1d7d7f9ddff67ee5a345
rony2k6 <rony2k6@gmail.com>
2018-11-05 09:14:28 +0600

    Service added till Observable data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,20 +10,26 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero; 
   // hero: Hero = {
   //   id: 1,
   //   name: 'Windstorm'
   // };
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  /* function to retrieve Heroes from HeroService */
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
